c028e11856dccc3eeedbdc12db1040a6f40804bb
emanuelefavero <emafaux89@gmail.com>
2023-08-01 18:01:53 +0200

    add singleton

---
+++ singleton.js
@@ -0,0 +1,28 @@
+// The singleton pattern ensures that only one instance of a class is created
+// A good example of a singleton is a Logger class, which could be used to log every activity of a user in a social network (like logins, logout, likes, etc.)
+class Logger {
+  constructor() {
+    // Check if there is already an instance created
+    if (!Logger.instance) {
+      // If not, create one
+      Logger.instance = this
+    }
+
+    // If there is already an instance created, return that instance
+    return Logger.instance
+  }
+
+  // A method to log a message
+  log(message) {
+    console.log('Message: ', message)
+  }
+}
+
+// TIP: Both logger1 and logger2 will use the same instance of the Logger class even though we are instantiating it twice
+const logger1 = new Logger()
+const logger2 = new Logger()
+
+console.log(logger1 === logger2) // true
+
+logger1.log('Hello from logger 1')
+logger2.log('Hello from logger 2')
